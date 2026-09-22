eddeeecba3424ae8824976d9cb92f6d0683c7bab
Gautam Mahajan <gautammahajan86@gmail.com>
2026-09-22 18:40:38 -0400

    Fixed stale files in Git

---
+++ app/labs/page.tsx
@@ -0,0 +1,13 @@
+export default function Labs() {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <h2>Name: Gautam M</h2>
+      <h2>Section: CS5610 02</h2>
+      
+      <a id="wd-github" href="https://github.com/GautamM86/webdev-client">
+        My GitHub Repository
+      </a>
+    </div>
+  );
+}
